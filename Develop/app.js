1c31495d8a08dfc8dbe59ac7b5822bc54f3a3f1b
Bobby Jones Jr <jones9682@yahoo.com>
2020-08-01 09:57:30 -0400

    added function and switch case to create teams

--- Develop/app.js
+++ Develop/app.js
@@ -9,16 +9,23 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Choices = require("inquirer/lib/objects/choices");
-const { type } = require("os");
 
+// function to build team members
+const teamMembers = [];
+function write() {
+    let path = render(teamMembers)
+    fs.writeFile(outputPath, path, function (error) {
+        if (error) {
+            console.log(error);
+            throw error;
+        };
+        console.log("The team.html file created success!");
+    })
+}
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-const teamMembers = []
-
+//
 function createTeam() {
-    inquirer.prompt([
+    return inquirer.prompt([
         {
             type: "list",
             name: "teamChoice",
@@ -30,27 +37,22 @@ function createTeam() {
                 'I do not want to add more team members'
             ]
         }
-
-    ]).then(userChoice => {
+    ]).then(function (userChoice) {
         switch (userChoice.teamChoice) {
             case "Manager":
                 addManager();
                 break;
-
             case "Engineer":
                 addEngineer();
                 break;
-
             case "Intern":
                 addIntern();
                 break;
-
-            case "No more team members":
-                render(teamMembers);
-                break
+            case "I do not want to add more team members":
+                write();
+                break;
         }
     })
-
     function addManager() {
         inquirer.prompt([
             {
@@ -81,95 +83,79 @@ function createTeam() {
                 createTeam();
             })
     }
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your engineer's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your engineer's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your engineer's email?"
+            },
+            {
+                type: "input",
+                name: "gitHub",
+                message: "What is your engineer's GitHub username?"
+            },
+        ])
+            .then(userChoice => {
+                const engineer = new Engineer(userChoice.name, userChoice.id, userChoice.email, userChoice.gitHub)
+                teamMembers.push(engineer)
+                console.log(teamMembers);
+                createTeam();
+            })
+    }
+    function addIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is your intern's name?"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "What is your intern's id?"
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "What is your intern's email?"
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "What is your intern's school?"
+            },
+        ])
+            .then(userChoice => {
+                const intern = new Intern(userChoice.name, userChoice.id, userChoice.email, userChoice.school)
+                teamMembers.push(intern)
+                console.log(teamMembers);
+                createTeam();
+            })
+    }
 }
-
-
-function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your engineer's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your engineer's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your engineer's email?"
-        },
-        {
-            type: "input",
-            name: "gitHub",
-            message: "What is your engineer's GitHub username?"
-        },
-    ])
-        .then(userChoice => {
-            const engineer = new Engineer(userChoice.name, userChoice.id, userChoice.email, userChoice.gitHub)
-            teamMembers.push(engineer)
-            console.log(teamMembers);
-            createTeam();
-        })
-}
-
-function addIntern() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "What is your intern's name?"
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "What is your intern's id?"
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "What is your intern's email?"
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "What is your intern's school?"
-        },
-    ])
-        .then(userChoice => {
-            const intern = new Intern(userChoice.name, userChoice.id, userChoice.email, userChoice.school)
-            teamMembers.push(intern)
-            console.log(teamMembers);
-            createTeam();
-        })
-}
-
-module.exports = teamMembers
-
 createTeam();
-
-
-
-// function to build team members
-
-
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
